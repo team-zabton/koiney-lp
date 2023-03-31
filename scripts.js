8c27c1fe8fe84f5fca8503928f3aaffc3699abd4
team-zabton <zabton.team@gmail.com>
2023-04-01 01:48:55 +0900

    contact window

--- scripts.js
+++ scripts.js
@@ -19,3 +19,17 @@
     });
   });
   
+  document.getElementById('open-modal').addEventListener('click', function() {
+    document.getElementById('modal').style.display = 'block';
+  });
+  
+  document.getElementById('close-modal').addEventListener('click', function() {
+    document.getElementById('modal').style.display = 'none';
+  });
+  
+  window.addEventListener('click', function(event) {
+    if (event.target == document.getElementById('modal')) {
+      document.getElementById('modal').style.display = 'none';
+    }
+  });
+  
